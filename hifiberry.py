ff5cd4930708348b4c83de3ad3f9e70a55403c0e
Olivier Gonson <olivier.gonson@gmail.com>
2024-01-04 15:16:29 -0500

    udpate test

---
+++ hifiberry.py
@@ -0,0 +1,11 @@
+import requests
+
+def get_status():
+    r = requests.get(url + "/api/track/metadata").json()
+    current = {
+        "type": r.["playerName"],
+        "title": r.["title"],
+        "artist": r.["artist"]
+    }
+    logging.debug(current)
+    return current
--- test.py
+++ test.py
@@ -2,12 +2,9 @@ import time
 import logging
 from display import Display
 import pictures as pics
+import currentberry as current
 
 # Display functions
-def get_bar(duration, seek):
-    nb_char = int((seek / duration) * 46)
-    return bar
-
 def get_formatted_title(title):
     if len(title) > 16:
         formatted_title = title[:13]  + "..."
@@ -20,22 +17,21 @@ def main():
 
     logging.basicConfig(filename='controller.log', level=logging.DEBUG)
 
-    # hifi = Volumio(url="http://localhost:3000")
-    hifi = {
+    current = {
         "type": "Spotify",
         "title": "Pump up the jam",
         "artist": "Technotronic"
     }
     disp = Display(LCD_CS=8,LCD_RST=25,LCD_A0=24,LCD_CLK=11,LCD_SI=10)
+    hifi_url = "http://127.0.0.1:81"
 
-#    try:
     while True:
-        # hifi.update_status()
-        logging.debug("type:" + hifi["type"] + " - title: " + hifi["title"])
+        current = hifi.get_status(hifi_url)
+        logging.debug("type:" + current["type"] + " - title: " + current["title"])
         disp.lcd_clear()
         logging.debug("updating display")
-        disp.lcd_ascii168_string(0, 4, get_formatted_title(hifi["title"]))
-        disp.lcd_ascii168_string(0, 6, hifi["artist"])
+        disp.lcd_ascii168_string(0, 4, get_formatted_title(current["title"]))
+        disp.lcd_ascii168_string(0, 6, current["artist"])
         disp.lcd_picture(0,0,pics.SPOTIFY,32)
         time.sleep(2)
 
