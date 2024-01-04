44cafc4e25eefe4d71a67c21e00debb6ac201b1c
Olivier Gonson <olivier.gonson@gmail.com>
2024-01-04 15:17:39 -0500

    udpate test

--- hifiberry.py
+++ hifiberry.py
@@ -3,9 +3,9 @@ import requests
 def get_status():
     r = requests.get(url + "/api/track/metadata").json()
     current = {
-        "type": r.["playerName"],
-        "title": r.["title"],
-        "artist": r.["artist"]
+        "type": r["playerName"],
+        "title": r["title"],
+        "artist": r["artist"]
     }
     logging.debug(current)
     return current
