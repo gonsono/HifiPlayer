78dfcb39d8183b42a7d8ba8beab21fe43b4ba700
Olivier Gonson <olivier.gonson@gmail.com>
2024-01-04 20:15:17 -0500

    udpate test

--- test.py
+++ test.py
@@ -31,10 +31,10 @@ def main():
         logging.debug("updating display")
         disp.lcd_ascii168_string(0, 4, get_formatted_title(current["title"]))
         disp.lcd_ascii168_string(0, 6, current["artist"])
-        disp.lcd_picture(6,0,pics.BT,20)
+        disp.lcd_picture(6,1,pics.BT,20)
         # disp.lcd_picture(2,0,pics.SPOT28,28)
         # disp.lcd_picture(0,0,pics.SPOTIFY,32)
-        disp.lcd_ascii168_string(50, 0, get_time())
+        disp.lcd_ascii168_string(50, 1, get_time())
         if current["state"] == "paused":
             disp.lcd_picture(112,1,pics.PAUSE,8)
         elif  current["state"] == "playing":
