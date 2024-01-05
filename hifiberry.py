dadd10f928af90c1a62e1f8db0d4a45a3c9ed35c
Olivier Gonson <olivier.gonson@gmail.com>
2024-01-04 23:09:50 -0500

    udpate test

--- hifiberry.py
+++ hifiberry.py
@@ -1,26 +1,68 @@
 import requests
 import time
+import logging
 
-def get_time():
-    current_time = time.strftime("%H:%M")
-    return current_time
+# def get_status(url, last_refresh):
+#     players = requests.get(url + "/api/player/status").json()
+#     playing = [p["state"] for p in players if p["state"]=="playing"]
+#     playing = [p["state"] for p in players if p["state"]=="playing"]
+#     if len(playing)==1:
+#         track = requests.get(url + "/api/track/metadata").json()
+#         if track["title"] is not None: 
+#             current = {
+#                 "type": track["playerName"],
+#                 "title": track["title"],
+#                 "artist": track["artist"],
+#                 "state": track["playerState"],
+#                 "time": get_time()
+#             }
+#         else:
+#             current = {
+#                 "type": "none",
+#                 "title": "",
+#                 "artist": "",
+#                 "state": "idle",
+#                 "time": get_time()
+#             }
+#     elif len(playing)==0 and last
+#     return current
 
-def get_status(url):
-    r = requests.get(url + "/api/track/metadata").json()
-    if r["title"] is not None:
-        current = {
-            "type": r["playerName"],
-            "title": r["title"],
-            "artist": r["artist"],
-            "state": r["playerState"],
-            "time": get_time()
-        }
-    else:
-        current = {
-            "type": "None",
-            "title": "Idle",
-            "artist": "",
-            "state": "",
-            "time": get_time()
-        }
-    return current
+
+
+class HifiBerry:
+    """Class that allow interaction with HifiBerry API"""
+
+    url = "http://localhost:81"
+
+    def __init__(self, url=url):
+        self.url = url
+        self.last_refresh = time.time()
+        if self.update_status():
+            logging.info("Connected to HifiBerry on " + self.url)
+
+    def update_status(self):
+        """Get status of HifiBerry"""
+        try:
+            players = requests.get(self.url+"/api/player/status").json()
+        except:
+            logging.error("Could not get list of players")
+            return False
+        
+        active = [p["state"] for p in players if p["state"]=="playing" or p["state"]=="paused"]
+        if len(active)==1:
+            try:
+                track = requests.get(self.url + "/api/track/metadata").json()
+            except:
+                logging.error("Could not get track metadata")
+                return False
+            self.type = track["playerName"]
+            self.title = track["title"]
+            self.artist = track["artist"]
+            self.state = track["playerState"]
+        else:
+            self.type = "none"
+            self.title = ""
+            self.artist = ""
+            self.state = "idle"
+        return True
+
--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import time
 import logging
 from display import Display
 import pictures as pics
-import hifiberry as hifi
+from hifiberry import HifiBerry
 
 # Display functions
 def get_formatted_title(title):
@@ -12,30 +12,39 @@ def get_formatted_title(title):
         formatted_title = title
     return formatted_title
 
+def get_time():
+    current_time = time.strftime("%H:%M")
+    return current_time
+
 # Main function
 def main():
 
     logging.basicConfig(filename='controller.log', level=logging.DEBUG)
     disp = Display(LCD_CS=8,LCD_RST=25,LCD_A0=24,LCD_CLK=11,LCD_SI=10)
-    hifi_url = "http://127.0.0.1:81"
-
+    hifi = HifiBerry()
     disp.lcd_clear()
-    current = ""
+
     while True:
-        if current != hifi.get_status(hifi_url):
-            current = hifi.get_status(hifi_url)
-            disp.lcd_clear()
-            disp.lcd_ascii168_string(0, 4, get_formatted_title(current["title"]))
-            disp.lcd_ascii168_string(0, 6, current["artist"])
-            if current["type"] == "spotify":
-                disp.lcd_picture(2,0,pics.SPOT28,28)
-            else:
-                disp.lcd_picture(6,1,pics.BT,20)
-            disp.lcd_ascii168_string(46, 1, current["time"])
-            if current["state"] == "paused":
-                disp.lcd_picture(112,1,pics.PAUSE,8)
-            elif  current["state"] == "playing":
-                disp.lcd_picture(112,1,pics.PLAY,8)
+        hifi.update_status()
+        disp.lcd_clear()
+        disp.lcd_ascii168_string(0, 4, get_formatted_title(hifi.title))
+        disp.lcd_ascii168_string(0, 6, hifi.artist)
+        
+        if hifi.type == "spotify":
+            disp.lcd_picture(2,0,pics.SPOT28,28)
+        elif hifi.type == "none":
+            disp.lcd_picture(6,1,pics.NOTE,20)
+        else:
+            disp.lcd_picture(6,1,pics.BT,20)
+
+        disp.lcd_ascii168_string(46, 1, get_time())
+
+        if hifi.state == "paused":
+            disp.lcd_picture(112,1,pics.PAUSE,8)
+        elif  hifi.state == "playing":
+            disp.lcd_picture(112,1,pics.PLAY,8)
+        else:
+            disp.lcd_picture(106,1,pics.HEART,20)
         time.sleep(2)
 
 main()
